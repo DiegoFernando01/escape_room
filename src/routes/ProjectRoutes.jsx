743d460b0231e55dc1ae1f8967edcff52e8680ed
DiegoFernando01 <diegofvictoria2013@gmail.com>
2023-12-12 23:13:10 -0500

    routes

--- src/routes/ProjectRoutes.jsx
+++ src/routes/ProjectRoutes.jsx
@@ -2,15 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import ChapterOne from "../pages/chapters/ChapterOne";
 import SignUp from "../pages/signup/SignUp";
 import Login from "../pages/login/Login";
+import Introduction from "../pages/introduction/IntroductionExp";
 
 const ProjectRoutes = () => {
   return (
     <Routes>
       <Route path="/">
         <Route index element={<ChapterOne />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/chapter_one" element={<ChapterOne />} />
+        <Route path="/introduction" element={<Introduction />} />
       </Route>
     </Routes>
   );
