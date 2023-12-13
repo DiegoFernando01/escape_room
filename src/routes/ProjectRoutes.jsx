3178a496bbae1533a5cefb0bc15841e5141947f8
DiegoFernando01 <diegofvictoria2013@gmail.com>
2023-12-13 10:02:40 -0500

    ChapterTwo routes updated

--- src/routes/ProjectRoutes.jsx
+++ src/routes/ProjectRoutes.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import ChapterOne from "../pages/chapters/ChapterOne";
+import ChapterTwo from "../pages/chapters/ChapterTwo";
 import SignUp from "../pages/signup/SignUp";
 import Login from "../pages/login/Login";
 
@@ -11,6 +12,7 @@ const ProjectRoutes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/chapter_one" element={<ChapterOne />} />
+        <Route path="/chapter_two" element={<ChapterTwo />} />
       </Route>
     </Routes>
   );
